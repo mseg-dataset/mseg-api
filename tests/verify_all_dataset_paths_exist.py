73a12ac0af0e2ec6d37b65c6a4ef039df34c9102
John Lambert <johnwlambert@gmail.com>
2020-05-15 00:40:09 -0400

    add exact pixel checks

--- tests/verify_all_dataset_paths_exist.py
+++ tests/verify_all_dataset_paths_exist.py
@@ -8,7 +8,10 @@ from mseg.utils.cv2_utils import (
 	cv2_imread_rgb
 )
 from mseg.utils.dataset_config import infos
-from mseg.utils.names_utils import get_dataloader_id_to_classname_map
+from mseg.utils.names_utils import (
+	get_classname_to_dataloaderid_map,
+	get_dataloader_id_to_classname_map
+)
 from mseg.utils.mask_utils import (
 	form_mask_triple_embedded_classnames,
 	save_classnames_in_image_maxcardinality
@@ -83,6 +86,8 @@ class SanityCheckDataset:
 		self.dataroot = dataroot
 		self.dname = dname
 		self.id_to_classname_map = get_dataloader_id_to_classname_map(dname)
+		self.get_classname_to_id_map = get_classname_to_dataloaderid_map(dataset_name=dname)
+
 
 	def find_matches(self, examples):
 		""" """
@@ -92,13 +97,15 @@ class SanityCheckDataset:
 			split_txt_fpath = f'{_ROOT}/mseg/dataset_lists/{self.dname}/list/{split}.txt'
 			pairs = generate_all_img_label_pair_fpaths(self.dataroot, split_txt_fpath)
 		
-			for (title, annot_fname, data_split) in examples:
+			for (title, annot_fname, data_split, coords) in examples:
 				parent, fname_stem, _ = get_unique_mask_identifiers(self.dname, annot_fname, data_split)
 				for (rgb_fpath, label_fpath) in pairs:
 					if Path(rgb_fpath).stem == fname_stem and Path(label_fpath).parts[-2] == parent:
 
 						rgb_img = imageio.imread(rgb_fpath)
 						label_img = imageio.imread(label_fpath)
+						expected_val = 
+						assert label_img[y,x] == self.get_classname_to_id_map[title]
 						save_fpath = f'{save_dir}/{self.dname}_{title}_{annot_fname}'
 						save_classnames_in_image_maxcardinality(
 							rgb_img, 
@@ -109,15 +116,41 @@ class SanityCheckDataset:
 							save_fpath=save_fpath)
 
 
+
 def verify_targeted_visual_examples():
 	"""
 	"""
 	cityscapes_examples = [
-		('bicycle', 'seqtubingen_tubingen_000060_000019_leftImg8bit_57.jpg', 'train'),
-		('bicyclist', 'seqhamburg_hamburg_000000_068916_leftImg8bit_223.jpg', 'train'),
-		('motorcyclist', 'seqbremen_bremen_000052_000019_leftImg8bit_102.jpg', 'train'),
-		('rider-other', 'seqaachen_aachen_000014_000019_leftImg8bit_84.jpg', 'train'),
-		('person-nonrider', 'seqfrankfurt_frankfurt_000001_038844_leftImg8bit_82.jpg', 'val')
+		[
+			'bicycle',
+			'seqtubingen_tubingen_000060_000019_leftImg8bit_57.jpg',
+			'train',
+			(474, 760)
+		],
+		[
+			'bicyclist',
+			'seqhamburg_hamburg_000000_068916_leftImg8bit_223.jpg',
+			'train',
+			(353, 1181) 
+		],
+		[
+			'motorcyclist',
+			'seqbremen_bremen_000052_000019_leftImg8bit_102.jpg',
+			'train',
+			(403, 1048)
+		],
+		[
+			'rider_other',
+			'seqaachen_aachen_000014_000019_leftImg8bit_84.jpg',
+			'train',
+			(542,531)
+		],
+		[
+			'person_nonrider',
+			'seqfrankfurt_frankfurt_000001_038844_leftImg8bit_82.jpg',
+			'val',
+			(341,649)
+		],
 	]
 	dname = 'cityscapes-19-relabeled'
 	
@@ -126,25 +159,25 @@ def verify_targeted_visual_examples():
 
 	dname = 'ade20k-150-relabeled'
 	ade20k_examples = [
-		('zebra', 'ADE_train_00016125_113.png', 'train'),
-		('nightstand', 'ADE_train_00004036_119.png', 'train'),
-		('wine glass', 'ADE_val_00001949_134.png', 'val'),
-		('bicyclist', 'ADE_train_00017384_120.png', 'train'),
-		('bicyclist', 'ADE_train_00017404_215.png', 'train'),
-		('counter-other', 'ADE_val_00001477_157.png', 'val'),
-		('teddy_bear', 'ADE_train_00001951_185.png', 'train')
+		['zebra', 'ADE_train_00016125_113.png', 'train', (506,315) ],
+		['nightstand', 'ADE_train_00004036_119.png', 'train', (195, 37) ],
+		['wine_glass', 'ADE_val_00001949_134.png', 'val', (300,478) ],
+		['bicyclist', 'ADE_train_00017384_120.png', 'train', (398, 55) ],
+		['bicyclist', 'ADE_train_00017404_215.png', 'train', (268, 373) ],
+		['counter_other', 'ADE_val_00001477_157.png', 'val', (160,234) ],
+		['teddy_bear', 'ADE_train_00001951_185.png', 'train', (328,508) ],
 	]
 	scd = SanityCheckDataset(infos[dname].dataroot, dname)
 	scd.find_matches(ade20k_examples)
 
 	dname = 'bdd-relabeled'
 	bdd_examples = [
-		('motorcyclist', 'a91b7555-00000590_12.jpg', 'val'),
-		('bicyclist', '3516379e-43f6a6ba_12.jpg', 'train'),
-		('motorcyclist', '972ab49a-6a6eeaf5_11.jpg', 'train'),
-		('person-nonrider', '8413f861-580b500d_12.jpg', 'train'),
-		('person-nonrider', 'a706da19-14468d02_11.jpg', 'val'),
-		( 'bicyclist', '1ff92f74-697a077e_11.jpg', 'train')
+		['motorcyclist', 'a91b7555-00000590_12.jpg', 'val', (470, 888) ],
+		['bicyclist', '3516379e-43f6a6ba_12.jpg', 'train', (410, 654) ],
+		['motorcyclist', '972ab49a-6a6eeaf5_11.jpg', 'train', (396, 284) ],
+		['person_nonrider', '8413f861-580b500d_12.jpg', 'train', (416,926) ],
+		['person_nonrider', 'a706da19-14468d02_11.jpg', 'val', (300,1248) ],
+		['bicyclist', '1ff92f74-697a077e_11.jpg', 'train',  (270, 946) ],
 	]
 	scd = SanityCheckDataset(infos[dname].dataroot, dname)
 	scd.find_matches(bdd_examples)
@@ -152,57 +185,57 @@ def verify_targeted_visual_examples():
 	dname = 'coco-panoptic-133-relabeled'
 	cocop_examples = [
 		# title, fname, split
-		('wheelchair', '000000091349_3093585.png', 'train'),
-		('seat', '000000016509_9079654.png', 'train'),
-		('bathroom counter', '000000376310_2631210.png', 'val'),
-		('bicyclist', '000000067208_4211027.png', 'train'),
-		('motorcyclist', '000000459634_6904408.png', 'val'),
-		('kitchen island', '000000438774_5403260.png', 'val')
+		['wheelchair', '000000091349_3093585.png', 'train', (245, 396) ],
+		['seat', '000000016509_9079654.png', 'train',  (26, 120) ],
+		['bathroom_counter', '000000376310_2631210.png', 'val', (418, 558) ],
+		['bicyclist', '000000067208_4211027.png', 'train', (83, 404) ],
+		['motorcyclist', '000000459634_6904408.png', 'val',  (299,99) ],
+		['kitchen_island', '000000438774_5403260.png', 'val', (199, 341) ],
 	]
 	scd = SanityCheckDataset(infos[dname].dataroot, dname)
 	scd.find_matches(cocop_examples)
 	
-	dname = 'idd-39-relabeled'
-	idd_examples = [
-		('backpack', 'seq173_318463_leftImg8bit_50.jpg', 'train'),
-		('bicyclist', 'seq158_867833_leftImg8bit_54.jpg', 'train'),
-		('box', 'seq173_316862_leftImg8bit_34.jpg', 'train'),
-		('motorcyclist', 'seq98_776293_leftImg8bit_110.jpg', 'train'),
-		('person-non-rider', 'seq1_148425_leftImg8bit_110.jpg', 'train'),
-		('rider-other', 'seq48_601929_leftImg8bit_79.jpg', 'val'),
-		('bicyclist', 'seq181_016419_leftImg8bit_41.jpg', 'val'),
-		('rider-other', 'seq148_000690_leftImg8bit_67.jpg', 'val')
-	]
-	scd = SanityCheckDataset(infos[dname].dataroot, dname)
-	scd.find_matches(idd_examples)
+	# dname = 'idd-39-relabeled'
+	# idd_examples = [
+	# 	('backpack', 'seq173_318463_leftImg8bit_50.jpg', 'train'),
+	# 	('bicyclist', 'seq158_867833_leftImg8bit_54.jpg', 'train'),
+	# 	('box', 'seq173_316862_leftImg8bit_34.jpg', 'train'),
+	# 	('motorcyclist', 'seq98_776293_leftImg8bit_110.jpg', 'train'),
+	# 	('person-non-rider', 'seq1_148425_leftImg8bit_110.jpg', 'train'),
+	# 	('rider-other', 'seq48_601929_leftImg8bit_79.jpg', 'val'),
+	# 	('bicyclist', 'seq181_016419_leftImg8bit_41.jpg', 'val'),
+	# 	('rider-other', 'seq148_000690_leftImg8bit_67.jpg', 'val')
+	# ]
+	# scd = SanityCheckDataset(infos[dname].dataroot, dname)
+	# scd.find_matches(idd_examples)
 
-	dname = 'sunrgbd-37-relabeled'
-	sunrgbd_examples = [
-		('swivel chair', 	'img-000253_4.jpg', 'train'),
-		('door', 			'img-004239_4.jpg', 'train'),
-		('desk', 			'img-004930_11.jpg', 'train'),
-		('bathroom counter','img-002347_11.jpg', 'train'),
-		('bathroom counter','img-002164_11.jpg', 'test'),
-		('sconce', 			'img-004159_34.jpg', 'train'),
-		('chandelier', 		'img-001692_34.jpg', 'train')
-	]
-	scd = SanityCheckDataset(infos[dname].dataroot, dname)
-	scd.find_matches(sunrgbd_examples)
+	# dname = 'sunrgbd-37-relabeled'
+	# sunrgbd_examples = [
+	# 	('swivel chair', 	'img-000253_4.jpg', 'train'),
+	# 	('door', 			'img-004239_4.jpg', 'train'),
+	# 	('desk', 			'img-004930_11.jpg', 'train'),
+	# 	('bathroom counter','img-002347_11.jpg', 'train'),
+	# 	('bathroom counter','img-002164_11.jpg', 'test'),
+	# 	('sconce', 			'img-004159_34.jpg', 'train'),
+	# 	('chandelier', 		'img-001692_34.jpg', 'train')
+	# ]
+	# scd = SanityCheckDataset(infos[dname].dataroot, dname)
+	# scd.find_matches(sunrgbd_examples)
 	
-	dname = 'mapillary-public65-relabeled'
-	mapillary_examples = [
-		('sea', 'mapillary_EZJTbIpLNOfHnw3DvA1NEA_7936.jpg', 'val'),
-		('fountain', 'mapillary_1fhI-qv6dVR-1Kc8Oafb3Q_7936.jpg', 'train'),
-		('horse', 'mapillary_CH1TcYj0ki_kw3Yc3oOiZA_258.jpg', 'train'),
-		('dog', 'mapillary_OkOJlU8l98KqPEE05dv1tA_256.jpg', 'val')
-	]
-	scd = SanityCheckDataset(infos[dname].dataroot, dname)
-	scd.find_matches(mapillary_examples)
+	# dname = 'mapillary-public65-relabeled'
+	# mapillary_examples = [
+	# 	('sea', 'mapillary_EZJTbIpLNOfHnw3DvA1NEA_7936.jpg', 'val'),
+	# 	('fountain', 'mapillary_1fhI-qv6dVR-1Kc8Oafb3Q_7936.jpg', 'train'),
+	# 	('horse', 'mapillary_CH1TcYj0ki_kw3Yc3oOiZA_258.jpg', 'train'),
+	# 	('dog', 'mapillary_OkOJlU8l98KqPEE05dv1tA_256.jpg', 'val')
+	# ]
+	# scd = SanityCheckDataset(infos[dname].dataroot, dname)
+	# scd.find_matches(mapillary_examples)
 
 
 if __name__ == '__main__':
 	verify_targeted_visual_examples()
-	visual_sanitychecks()
-	verify_all_dataset_paths_exist()
+	#visual_sanitychecks()
+	#verify_all_dataset_paths_exist()
 	
 
