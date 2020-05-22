bb7e5fca7dace8b626e6955561d5bbc2d61f67f6
John Lambert <johnwlambert@gmail.com>
2020-05-22 17:12:49 -0400

    remove mseg dst dir

--- mseg/utils/dataset_config.py
+++ mseg/utils/dataset_config.py
@@ -9,7 +9,7 @@ from mseg.utils.names_utils import load_class_names
 
 # -------------------------------------------------------------
 # You must specify the parent directory of `mseg_dataset` below:
-MSEG_DST_DIR = "/export/share/Datasets/MSegV4"
+MSEG_DST_DIR = ""
 # -------------------------------------------------------------
 if MSEG_DST_DIR == '':
 	raise Exception(
